2345fe7ccb14fdcb3cd277fe434229f6f0f3c4bb
Ernest <sacdalernest01@gmail.com>
2024-08-16 15:31:46 +0800

    2

--- dist/types/db_types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=db_types.js.map
--- dist/utils/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const globalForPrisma = globalThis;
-const prisma = globalForPrisma.prisma || new client_1.PrismaClient();
-if (process.env.PROJECT_ENV !== "production")
-    globalForPrisma.prisma = prisma;
-exports.default = prisma;
-//# sourceMappingURL=client.js.map
